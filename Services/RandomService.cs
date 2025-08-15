36811b8ff3436cadf0b5aab643499475902fa004
Fábio André Campos da Cruz <faccruz@gmail.com>
2025-08-15 20:10:05 -0300

    - Correção do random - Validação se o número já está cadastrado.

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -7,19 +7,29 @@ namespace ProvaPub.Services
 	public class RandomService
 	{
 		int seed;
+        bool numeroRepetido = false;
         TestDbContext _ctx;
 		public RandomService()
         {
             var contextOptions = new DbContextOptionsBuilder<TestDbContext>()
     .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Teste;Trusted_Connection=True;")
     .Options;
-            seed = Guid.NewGuid().GetHashCode();
+            
 
             _ctx = new TestDbContext(contextOptions);
         }
         public async Task<int> GetRandom()
 		{
+            seed = Guid.NewGuid().GetHashCode();
             var number =  new Random(seed).Next(100);
+
+            do  {
+                
+                number = new Random(seed).Next(100);
+                numeroRepetido = _ctx.Numbers.Any(n => n.Number == number);
+            } while (numeroRepetido);
+
+           
             _ctx.Numbers.Add(new RandomNumber() { Number = number });
             _ctx.SaveChanges();
 			return number;
